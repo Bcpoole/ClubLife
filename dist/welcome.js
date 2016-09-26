6bf755339486a29dfe083c1b800bf4215eff892c
Bcpoole <brandoncpoole@gmail.com>
2016-09-26 13:47:32 -0500

    Added logo and transparent version to assets. Added basic goals and logo to Welcome page

--- dist/welcome.js
+++ dist/welcome.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 System.register([], function (_export, _context) {
   "use strict";
@@ -14,13 +14,14 @@ System.register([], function (_export, _context) {
   return {
     setters: [],
     execute: function () {
-      _export('Welcome', Welcome = function Welcome() {
+      _export("Welcome", Welcome = function Welcome() {
         _classCallCheck(this, Welcome);
 
         this.heading = 'ClubLife';
+        this.goals = "Major goals include developing the app to be cross-platform through the use of Xamarin, providing user-centered functionality that make it actually worth using, and having an easy-to-adopt setup for clubs.";
       });
 
-      _export('Welcome', Welcome);
+      _export("Welcome", Welcome);
     }
   };
 });
--- src/welcome.js
+++ src/welcome.js
@@ -2,4 +2,5 @@
 
 export class Welcome {
   heading = 'ClubLife';
+  goals = "Major goals include developing the app to be cross-platform through the use of Xamarin, providing user-centered functionality that make it actually worth using, and having an easy-to-adopt setup for clubs.";
 }
