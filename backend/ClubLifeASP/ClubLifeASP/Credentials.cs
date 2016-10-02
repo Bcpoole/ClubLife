197acad7401c3fd751b92b62db09949b479e6158
Brandon Poole <brandoncpoole@gmail.com>
2016-10-01 22:21:48 -0500

    Added MongoDB driver and a credential template

---
+++ backend/ClubLifeASP/ClubLifeASP/Credentials.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubLifeASP {
+    public static class Credentials {
+        public static MongoCredential mongoCredential = MongoCredential.CreateCredential("database", "user", "pass");
+        public static string mongoURL = "url";
+        public static int mongoPort = 27017;
+    }
+}
