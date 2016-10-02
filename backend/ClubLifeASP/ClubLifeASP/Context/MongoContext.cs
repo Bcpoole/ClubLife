35bfa6a8c86f09611546d12c4982c7bcad96a3dc
Brandon Poole <brandoncpoole@gmail.com>
2016-10-02 18:52:49 -0500

    Added FindOrganizationByName, FindOrganizationByDayAndTime (optional time parameter), and FindOrganizationByTag functions to context

--- backend/ClubLifeASP/ClubLifeASP/Context/MongoContext.cs
+++ backend/ClubLifeASP/ClubLifeASP/Context/MongoContext.cs
@@ -25,7 +25,31 @@ namespace ClubLifeASP.Context {
         }
 
         public IEnumerable<Organization> GetOrganizations() {
-            return database.GetCollection<Organization>("organizations").AsQueryable().ToList();
+            return database.GetCollection<Organization>("organizations").AsQueryable();
+        }
+
+        public IEnumerable<Organization> FindOrganizationByName(string name) {
+            var orgs = GetOrganizations();
+            return orgs.Where(x => x.name.ToLower().Contains(name.ToLower()));
+        }
+
+        //TODO: Make day into an Enumerable
+        //TODO: clean up the "Organization Meeting Time" on Mongo then fix this function
+        public IEnumerable<Organization> FindOrganizationByDayAndTime(string day, string time = null) {
+            var orgs = GetOrganizations();
+
+            //Just by day of week
+            if (time == null) {
+                return orgs.Where(x => x.OrganizationMeetingDay.Contains(day));
+            } else { //day and time
+                var orgsOnDay = orgs.Where(x => x.OrganizationMeetingDay.Contains(day));
+                return orgsOnDay.Where(x => x.OrganizationMeetingTime.Contains(time));
+            }
+        }
+
+        public IEnumerable<Organization> FindOrganizationByTag(string tag) {
+            var orgs = database.GetCollection<Organization>("organizations").AsQueryable().ToList();
+            return orgs.Where(x => x.MainSummary.ToLower().Contains(tag.ToLower()));
         }
     }
 }
