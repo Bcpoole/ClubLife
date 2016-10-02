9a77e0c3cf3251361b031a8f35d75593ee4a8e84
Brandon Poole <brandoncpoole@gmail.com>
2016-10-01 22:28:40 -0500

    Added MongoDB Context

---
+++ backend/ClubLifeASP/ClubLifeASP/Context/MongoContext.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubLifeASP.Context {
+    public class MongoContext {
+        private IMongoDatabase database;
+
+        public MongoContext() {
+            var credential = Credentials.mongoCredential;
+            var settings = new MongoClientSettings {
+                Credentials = new[]
+                {
+                    credential
+                },
+                Server = new MongoServerAddress(Credentials.mongoURL, Credentials.mongoPort)
+            };
+
+            var client = new MongoClient(settings);
+
+            database = client.GetDatabase("clublife-db");
+        }
+    }
+}
