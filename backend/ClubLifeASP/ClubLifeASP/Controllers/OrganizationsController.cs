6499bfa9a503951f2d61016486fb998d6b4feb07
Brandon Poole <brandoncpoole@gmail.com>
2016-10-09 17:08:18 -0500

    Added Autofac for IoC. Setup Autofac and added a controller for Organizations with the getall setup

---
+++ backend/ClubLifeASP/ClubLifeASP/Controllers/OrganizationsController.cs
@@ -0,0 +1,23 @@
+﻿using ClubLifeASP.Context;
+using ClubLifeASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClubLifeASP.Controllers
+{
+    public class OrganizationsController : ApiController
+    {
+        MongoContext mongoContext;
+        public OrganizationsController(MongoContext mongoContext) {
+            this.mongoContext = mongoContext;
+        }
+
+        public IEnumerable<Organization> GetAllOrganizations() {
+            return mongoContext.GetOrganizations();
+        }
+    }
+}
