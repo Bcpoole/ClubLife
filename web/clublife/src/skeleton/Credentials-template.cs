7f34baa810d0a32c65c14fe71057816353028f9b
Brandon Poole <brandoncpoole@gmail.com>
2016-11-03 12:27:07 -0500

    Gave Credentials-template.cs a different class name to prevent conflicts in event that this file is not deleted by user

--- web/clublife/src/skeleton/Credentials-template.cs
+++ web/clublife/src/skeleton/Credentials-template.cs
@@ -2,7 +2,7 @@ using MongoDB.Driver;
 
 namespace skeleton
 {
-    public static class Credentials {
+    public static class CredentialsTemplate {
         public static MongoCredential mongoCredential = MongoCredential.CreateCredential("db", "user", "pw");
         public static string mongoURL = "url";
         public static int mongoPort = 27017;
