d23a67cf36e81c580b59c687bf43450651c06775
Brandon Poole <brandoncpoole@gmail.com>
2016-11-11 12:31:16 -0600

    added uesr model

---
+++ web/clublife/src/skeleton/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace skeleton.Models {
+  [BsonIgnoreExtraElements]
+  public class User {
+    public ObjectId Id { get; set; }
+    public string Username { get; set; }
+    public IEnumerable<Organization> Clubs { get; set; }
+  }
+}
