12ca596a3114ca4078dc0c2e6abd80d038b24ff4
JonathanMerklin <jonathanmerklin@gmail.com>
2016-11-29 22:29:10 -0600

    Refactor the hell out of and fix edit club info

--- mobile/ClubLife/src/views/EditClub.js
+++ mobile/ClubLife/src/views/EditClub.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Alert,
   AppRegistry,
   StyleSheet,
   Text,
@@ -10,36 +11,12 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-
-
-//import TabNavigator from 'react-native-tab-navigator';
-//import Communications from 'react-native-communications';
-
+import Button from 'react-native-button';
 
 class EditClub extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            hasData: false,
-            data: [],
-            newVal: '',
-            currentVal:'',
-            newValues : {}
-
-        };
-        //this.setState({hasValues:false});
-    }
-
-
-
-    _resultsView(){
-        var TouchableElement = TouchableNativeFeedback;
-        var officer = true; // figure this out later
-
-        var data = this.state.data;
-        var yo = this;
-
-        var variableNames = ({'secondaryAdvisorDepartment':'Secondary Advisor Department','organizationMeetingLocation':'Meeting Location',
+        this.variableNames = ({'secondaryAdvisorDepartment':'Secondary Advisor Department','organizationMeetingLocation':'Meeting Location',
         'vicePresidentEmail':'Vice-President Email','presidentEmail':'President Email','parentOrganization':'Parent Organization',
         'organizationMeetingTime':'Meeting Time','advisorEmail':'Advisor Email','vicePresidentName':'Vice-President',
         'advisorPhone':'Advisor Phone','organizationEmail':'Organization Email','secretaryName':'Secretary',
@@ -48,180 +25,78 @@ class EditClub extends Component {
         'secondaryAdvisorPhone':'Secondary Advisor Phone','summary':'Summary','treasurerEmail':'Treasurer Email',
         'secondaryAdvisorEmail':'Secondary Advisor Email','mainSummary':'Main Summary','aboutSummary':'About Summary',
         'name':'Club Name','presidentName':'President'});
+        this.initial = {};
+        for(let key in this.variableNames) {
+            this.initial[key] = this.props.route.state.club[key];
+        }
+        this.state = this.initial;
+    }
 
+    _resultsView(){
+        var data = this.state.data;
 
-        function hey(changedVal,originalVal){
-            //if (boo in this.state.newValues){
-            if (changedVal in yo.state.newValues){
-                return "RAWWWR";
-                //return yo.state.newValues[changedVal];
-            }
-            else{
-                return originalVal;
-            }
-         }
-
-
-        var clubValue= ()=>{
-
-            var returnValue = [];
-            //this.state.newValues;
-                //onChangeText={(text)=>{this.state.newValues[i]=text}}
-                //this.state.newValue.push(club[prop]);
-            data.map(club=> {
-                var i = 0;
-
-                for (let prop in club){
-                        //this.state.newValues;
-
-                        if (prop==="id" || prop === "events" || prop==="posts" || prop==="img"){
-                            continue;
+        var retArr = [];
+        var i = 0;
+        for(let key in this.variableNames) {
+            retArr.push(
+                <View key={"input"+i}>
+                    <Text style={styles.welcome}>
+                        {this.variableNames[key]}:
+                    </Text>
+                    <TextInput
+                        style = {styles.textEdit}
+                        onChangeText={(newText)=>{
+                            let newStateObject = {};
+                            newStateObject[key] = newText; //{key: blah} doesn't work because state."key" literally
+                            this.setState(newStateObject);
+                            }
                         }
-
-
-                        returnValue.push(
-                            <View>
-                                <Text style={styles.welcome}>
-                                    {variableNames[prop]}:
-                                </Text>
-                                <TextInput
-                                    style = {styles.textEdit}
-                                    onChangeText={(newText)=>{
-
-                                        var obj = Object.assign(this.state.newValues,{prop:newText});
-                                        //this.state.newValues[club[prop]] = newText;
-                                        this.setState({newValues:obj});
-                                        }
-                                    }
-
-                                    value = {hey(prop,club[prop])}
-                                     >
-                                </TextInput>
-                            </View>);
-                        i++;                }
-
-                }, this) // end of map
-            return returnValue;
-
+                        defaultValue = {this.state[key]}
+                    />
+                </View>
+            );
+            i++;
         }
 
-        // var content = (<ScrollView style = {{marginTop: 30, paddingBottom: 30}}>
-
-        //     {clubValue()}
-        //     <Text>{this.state.newValues[0]}</Text>
-        //     <TouchableElement style = {styles.button} onPress = {()=>{alert(blah[1])}}>
-
-        // }
-
-        var content = (<ScrollView style = {{marginTop: 40, paddingBottom: 30}}>
-
-            {clubValue()}
-            <Text>{this.state.newValues[0]}</Text>
-            <TouchableElement style = {styles.button} onPress = {()=>{alert(blah[1])}}>
-                <View><Text>Submit</Text></View>
-            </TouchableElement>
-            <TouchableElement style = {styles.button} onPress = {()=>this._onGoClub()}>
-                <View><Text>Back</Text></View>
-            </TouchableElement>
-
-        </ScrollView>);
-
-       return content;
-
-
+        return retArr;
     }
 
-
-    _processSubmit(){
-        var jsonString = '{';
-        var id;
-        data.map(club=>{
-            id = club.id;
-            for(let prop in club){
-                if (prop in this.state.newValues){
-                    jsonString+="\"" + prop + "\":" + this.state.newValues[prop] + ",";
-                }
-                else{
-                    jsonString+="\"" + prop + "\":" + club[prop] + ",";
-                }
-
-            }
-        })
-        jsonString+='}';
-
-        var updatedData = JSON.parse(jsonString);
-
-        const url = "http://skeleton20161103012840.azurewebsites.net/api/organizations/name?name="+
-                this.props.route.state.club.name.replace(" ","+");
-        fetch(url,
-            {
-                method: "POST",
-                body: updatedData
-            })
-            .then(function(res){ return res.json(); })
-            .then(function(data){ alert( JSON.stringify( data ) ) })
-
-
-
-    }
-
-
-    _processSubmit(){
-        var jsonString = '{';
-        var id;
-        data.map(club=>{
-            id = club.id;
-            for(let prop in club){
-                if (prop in this.state.newValues){
-                    jsonString+="\"" + prop + "\":" + this.state.newValues[prop] + ",";
-                }
-                else{
-                    jsonString+="\"" + prop + "\":" + club[prop] + ",";
-                }
-
-            }
-        })
-        jsonString+='}';
-
-        var updatedData = JSON.parse(jsonString);
-
-        const url = "http://skeleton20161103012840.azurewebsites.net/api/organizations/name?name="+
-                this.props.route.state.club.name.replace(" ","+");
-        fetch(url,
-            {
-                method: "POST",
-                body: updatedData
-            })
-            .then(function(res){ return res.json(); })
-            .then(function(data){ alert( JSON.stringify( data ) ) })
-
-    }
-
-
-
-
-
-    render() {
-        return this._resultsView();
-
-    }
-        // Jonathan's component code
-    componentDidMount() {
-        const url = "http://skeleton20161103012840.azurewebsites.net/api/organizations/name?name="+
-                this.props.route.state.club.name.replace(" ","+");
-        fetch(url)
-            .then(res=>res.json())
+    _processSubmit() {
+        let club = this.props.route.state.club;
+        let url = "http://skeleton20161103012840.azurewebsites.net/api/organizations/"+club.id;
+        let body = Object.assign({}, club, this.state);
+        let headers = {'Accept': 'application/json', 'Content-Type': 'application/json'};
+        let parseResponse = res => res.text().then(text => text ? JSON.parse(text) : {});
+        console.log(url);
+        console.log(JSON.stringify(body));
+        fetch(url, {method: "POST", headers: headers, body: JSON.stringify(body)})
+            .then(parseResponse)
             .then(json => {
-                this.setState({
-                    hasData: true,
-                    data: json
-                })
+                this._onGoClub();
             })
             .catch(e => {
-                console.error(e);
-                //TODO: figure out how to navigate back out if something went wrong
-            })
-        }
+                Alert.alert("error","error updating club", [{"text": "leave", onPress: () => this._onGoClub()}]);
+            });
+    }
+
+    render() {
+        var TouchableElement = TouchableNativeFeedback;
+        return (
+            <View style={{paddingTop: 40}}>
+                <View>
+                    <TouchableElement style = {styles.button} onPress = {()=>this._processSubmit()}>
+                        <View><Text style={{color: 'blue'}}>Submit</Text></View>
+                    </TouchableElement>
+                    <TouchableElement style = {styles.button} onPress = {()=>this._onGoClub()}>
+                        <View><Text style={{color: 'red'}}>Back</Text></View>
+                    </TouchableElement>
+                </View>
+                <ScrollView>
+                    {this._resultsView()}
+                </ScrollView>
+            </View>
+        )
+    }
 
     _onGoClub() {
         this.props.navigator.push({
@@ -234,7 +109,7 @@ class EditClub extends Component {
 
 }
 
-   module.exports = EditClub;
+module.exports = EditClub;
 
 
 
